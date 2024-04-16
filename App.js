692d9258ba669351b7fe6ddcb88ba82fe16c5dac
kunjal56 <129360247+kunjal56@users.noreply.github.com>
2024-04-16 17:15:53 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import Home from './Home';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
